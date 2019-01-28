73aff6cf04cd51e7a477bd08e86784bf739c8a01
Fabio Colacio <fabio.colacio.dev@gmail.com>
2019-01-27 21:41:21 -0800

    Use templates to avoid unnecessary requests

--- library.go
+++ library.go
@@ -4,7 +4,8 @@ import(
     "github.com/fabiocolacio/liblit/cbz"
     "net/http"
     "encoding/json"
-    "io/ioutil"
+    "encoding/base64"
+    "html/template"
     "log"
 )
 
@@ -14,24 +15,37 @@ type Book struct {
     Author         string `json:"author"`
     Contributors   string `json:"contributors"`
     Subjects       string `json:"subjects"`
-    Cover        []byte   `json:"cover"`
+    Cover          string `json:"cover"`
     Id             int    `json:"id"`
 }
 
-var(
-    libraryPage []byte
-)
-
-func init() {
-    var err error
-    libraryPage, err = ioutil.ReadFile("html/library.htm")
-    if err != nil {
-        log.Fatal("Failed to load library.htm")
-    }
-}
-
 func libraryHandler(res http.ResponseWriter, req *http.Request) {
-    res.Write(libraryPage)        
+    t, err := template.ParseFiles("html/library.htm")
+    if err != nil {
+        res.WriteHeader(500)
+        log.Println(err)
+        return
+    }
+
+    var books []Book
+    for id, path := range settings.Books {
+        pages, err := cbz.NewFromFile(path)
+        if err != nil {
+            res.WriteHeader(500)
+            log.Println(err)
+            return
+        }
+
+        book := Book {
+            Type: "comic",
+            Cover: base64.StdEncoding.EncodeToString(pages[0]),
+            Id: id,
+        }
+
+        books = append(books, book)
+    }
+
+    t.Execute(res, books)
 }
 
 func getBooksHandler(res http.ResponseWriter, req *http.Request) {
@@ -45,7 +59,7 @@ func getBooksHandler(res http.ResponseWriter, req *http.Request) {
 
         book := Book {
             Type: "comic",
-            Cover: pages[0],
+            Cover: base64.StdEncoding.EncodeToString(pages[0]),
             Id: id,
         }
 
