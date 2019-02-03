fe950f7d2886a1963f519a20bb8f3dbbb240469f
Fabio Colacio <fabio.colacio.dev@gmail.com>
2019-02-02 20:08:45 -0800

    add tls support

---
+++ auth.go
@@ -0,0 +1,144 @@
+package main
+
+import(
+    "golang.org/x/crypto/scrypt"
+    "context"
+    "net/http"
+    "html/template"
+    "log"
+    "crypto/subtle"
+    "crypto/rand"
+//    "crypto/hmac"
+    "github.com/mongodb/mongo-go-driver/mongo/options"
+    "github.com/mongodb/mongo-go-driver/bson"
+)
+
+var(
+    hmacKey []byte
+)
+
+func init() {
+    hmacKey := make([]byte, 32)
+    if _, err := rand.Read(hmacKey); err != nil {
+        log.Fatal(err)
+    }
+}
+
+type User struct {
+    Username     string `bson:"username" json:"saltedhash"`
+    Salt       []byte   `bson:"salt" json:"saltedhash"`
+    SaltedHash []byte   `bson:"saltedhash" json:"saltedhash"`
+}
+
+const(
+    scryptN int = 32768
+    scryptR int = 8
+    scryptP int = 1
+    hashLen int = 32
+    saltLen int = 16
+)
+
+func HashAndSaltPassword(password, salt []byte) ([]byte, error) {
+    return scrypt.Key(password, salt, scryptN, scryptR, scryptP, hashLen)
+}
+
+func registerPageHandler(res http.ResponseWriter, req *http.Request) {
+    t, err := template.ParseFiles("html/auth.html")
+    if err != nil {
+        res.WriteHeader(500)
+        log.Println(err)
+        return
+    }
+
+    vals := struct{ Action, Name string }{ "/registeruser", "Register" }
+    if err = t.Execute(res, vals); err != nil {
+        log.Println(err)
+        return
+    }
+}
+
+func registerUserHandler(res http.ResponseWriter, req *http.Request) {
+    req.ParseForm()
+    username := req.FormValue("username")
+    password := req.FormValue("password")
+
+    salt := make([]byte, saltLen)
+    if _, err := rand.Read(salt); err != nil {
+        res.WriteHeader(500)
+        log.Println(err)
+        return
+    }
+
+    saltedHash, err := HashAndSaltPassword([]byte(password), salt)
+    if err != nil {
+        res.WriteHeader(500)
+        log.Println(err)
+        return
+    }
+
+    user := User {
+        Username: username,
+        Salt: salt,
+        SaltedHash: saltedHash,
+    }
+
+    users := db.Collection("users")
+    users.InsertOne(context.Background(), user)
+}
+
+func loginPageHandler(res http.ResponseWriter, req *http.Request) {
+    t, err := template.ParseFiles("html/auth.html")
+    if err != nil {
+        res.WriteHeader(500)
+        log.Println(err)
+        return
+    }
+
+    vals := struct{ Action, Name string }{ "/loginuser", "Login" }
+    if err = t.Execute(res, vals); err != nil {
+        log.Println(err)
+        return
+    }
+}
+
+func loginUserHandler(res http.ResponseWriter, req *http.Request) {
+    req.ParseForm()
+    username := req.FormValue("username")
+    password := req.FormValue("password")
+
+    var user User
+    err := usersCollection.FindOne(
+        context.Background(),
+        bson.D{{"username", username}},
+        options.FindOne(),
+    ).Decode(&user)
+    if err != nil {
+        res.WriteHeader(400)
+        res.Write([]byte("Invalid username or password."))
+        return
+    }
+
+    saltedHash, err := HashAndSaltPassword([]byte(password), user.Salt)
+    if err != nil {
+        res.WriteHeader(500)
+        log.Println(err)
+        return
+    }
+
+    if subtle.ConstantTimeCompare(saltedHash, user.SaltedHash) == 1 {
+        cookie := http.Cookie {
+            Name: "session",
+            Value: username,
+            Secure: true,
+            SameSite: 1,
+            Path: "/",
+        }
+
+        http.SetCookie(res, &cookie)
+        res.Write([]byte("login successful"))
+    } else {
+        res.WriteHeader(400)
+        res.Write([]byte("Invalid username or password."))
+    }
+}
+
--- main.go
+++ main.go
@@ -4,16 +4,82 @@ import(
     "net/http"
     "log"
     "io/ioutil"
+    "crypto/tls"
+    "strings"
+    "flag"
+    "fmt"
+)
+
+const(
+    httpAddr  string = ":8080"
+    httpsAddr string = ":9090"
 )
 
 func main() {
-    http.HandleFunc("/html/",        htmlHandler)
-    http.HandleFunc("/register",     registerPageHandler)
-    http.HandleFunc("/registeruser", registerUserHandler)
-    http.HandleFunc("/login",        loginPageHandler)
-    http.HandleFunc("/loginuser",    loginUserHandler)
+    var (
+        certFile    string
+        privKeyFile string
+    )
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    flag.StringVar(&certFile, "c", "", "The location of your ssl certificate")
+    flag.StringVar(&privKeyFile, "p", "", "The location of your ssl private key")
+    flag.Parse()
+
+    if certFile == "" {
+        log.Fatal("No certificate specified")
+    }
+    if privKeyFile == "" {
+        log.Fatal("No private key specified")
+    }
+
+    httpsMux := http.NewServeMux()
+    httpsMux.HandleFunc("/html/",        htmlHandler)
+    httpsMux.HandleFunc("/register",     registerPageHandler)
+    httpsMux.HandleFunc("/registeruser", registerUserHandler)
+    httpsMux.HandleFunc("/login",        loginPageHandler)
+    httpsMux.HandleFunc("/loginuser",    loginUserHandler)
+
+    httpsHandler := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+        res.Header().Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+        httpsMux.ServeHTTP(res, req)
+    })
+
+    httpsServer := &http.Server {
+        Addr: httpsAddr,
+        Handler: httpsHandler,
+        TLSConfig: &tls.Config {
+            MinVersion: tls.VersionTLS12,
+            PreferServerCipherSuites: true,
+            CipherSuites: []uint16 {
+                tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+                tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+                tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+            },
+        },
+    }
+
+    e := make(chan error)
+
+    go func() {
+        e <- http.ListenAndServe(httpAddr, http.HandlerFunc(tlsRedirectHandler))
+    }()
+
+    go func() {
+        e <- httpsServer.ListenAndServeTLS(certFile, privKeyFile)
+    }()
+
+    log.Println(<-e)
+}
+
+func tlsRedirectHandler(res http.ResponseWriter, req *http.Request) {
+    host := strings.Split(req.Host, ":")[0]
+    port := strings.Split(httpsAddr, ":")[1]
+    path := req.URL.Path
+
+    dest := fmt.Sprintf("https://%s:%s%s", host, port, path)
+    log.Printf("Redirecting HTTP client '%s' to %s", req.RemoteAddr, dest)
+    http.Redirect(res, req, dest, http.StatusTemporaryRedirect)
 }
 
 func htmlHandler(res http.ResponseWriter, req *http.Request) {
